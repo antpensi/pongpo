a07b3ec04da39e6244922223c1200952303a9b29
Anthony Pensiero <antpen154@gmail.com>
2016-10-24 17:16:37 -0400

    change favicon

--- bundle.js
+++ bundle.js
@@ -50,7 +50,7 @@
 	
 	var _game2 = _interopRequireDefault(_game);
 	
-	var _board = __webpack_require__(2);
+	var _board = __webpack_require__(3);
 	
 	var _board2 = _interopRequireDefault(_board);
 	
@@ -79,7 +79,7 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
-	var _board = __webpack_require__(2);
+	var _board = __webpack_require__(3);
 	
 	var _board2 = _interopRequireDefault(_board);
 	
@@ -297,7 +297,8 @@
 	exports.default = Game;
 
 /***/ },
-/* 2 */
+/* 2 */,
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -308,7 +309,7 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
-	var _square = __webpack_require__(3);
+	var _square = __webpack_require__(4);
 	
 	var _square2 = _interopRequireDefault(_square);
 	
@@ -537,7 +538,7 @@
 	}
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -722,4 +723,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=bundle.js.maple.js.map
+//# sourceMappingURL=bundle.js.map
