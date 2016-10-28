283b95676816934f5482adaed94b3afd04ee427d
Anthony Pensiero <antpen154@gmail.com>
2016-10-28 16:36:03 -0400

    bundle

--- bundle.js
+++ bundle.js
@@ -50,7 +50,7 @@
 	
 	var _game2 = _interopRequireDefault(_game);
 	
-	var _board = __webpack_require__(3);
+	var _board = __webpack_require__(2);
 	
 	var _board2 = _interopRequireDefault(_board);
 	
@@ -79,7 +79,7 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
-	var _board = __webpack_require__(3);
+	var _board = __webpack_require__(2);
 	
 	var _board2 = _interopRequireDefault(_board);
 	
@@ -223,6 +223,12 @@
 				if (this.board.isFullBoard()) {
 					$('.paused').text('Game Over!');
 					clearInterval(window.intervalId);
+					var gameOverDiv = $('<div></div>');
+					var $ul = $(".game-board");
+					var gameOverText = 'Game Over \nYour Score: ' + this.board.score;
+					gameOverDiv.text('' + gameOverText);
+					gameOverDiv.addClass("game-over-div slideDown");
+					$ul.append(gameOverDiv);
 				} else {
 					this.board.fillRandomSquare();
 				}
@@ -303,8 +309,7 @@
 	exports.default = Game;
 
 /***/ },
-/* 2 */,
-/* 3 */
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -315,7 +320,7 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
-	var _square = __webpack_require__(4);
+	var _square = __webpack_require__(3);
 	
 	var _square2 = _interopRequireDefault(_square);
 	
@@ -543,7 +548,7 @@
 	}
 
 /***/ },
-/* 4 */
+/* 3 */
 /***/ function(module, exports) {
 
 	'use strict';
